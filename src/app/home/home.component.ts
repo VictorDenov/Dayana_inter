28a109f6a1070cf06ef380976e2cae2d73f01522
VictorDenov <victor.guaracaa@gmail.com>
2025-01-30 12:33:50 -0500

    subida
    scsccs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { routes } from '../shared/routes/routes';
 
 @Component({
@@ -6,9 +6,45 @@ import { routes } from '../shared/routes/routes';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit, OnDestroy {
 
-    public routes = routes;
+  public routes = routes;
 
+  // Array de imágenes para el carousel
+  images = [
+    'assets/fondosits/baner.jpg',
+    'assets/fondosits/banner insti-02.jpg',
+    'assets/fondosits/banner insti-03.jpg'
+  ];
+
+  // Índice de la imagen actual
+  currentImageIndex = 0;
+  translateX = '0';
+
+  // Intervalo para cambiar las imágenes
+  intervalId: any;
+
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId); // Detener el carousel cuando se destruya el componente
+    }
+  }
+
+  // Iniciar el carousel
+  startCarousel() {
+    this.intervalId = setInterval(() => {
+      this.changeImage();
+    }, 5000); // Cambiar imagen cada 5 segundos
+  }
+
+  // Cambiar la imagen
+  changeImage() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    this.translateX = `-${100 * this.currentImageIndex}%`; // Mueve el contenedor hacia la izquierda
+  }
 }
